d9bbdeaf2ce095a06596d4f208115d72850a2110
Yaroslav <yaroslav.klo2005@gmail.com>
2025-03-01 23:08:55 +0200

    updated

--- dotnetLabs/Program.cs
+++ dotnetLabs/Program.cs
@@ -24,7 +24,10 @@ List<Plane> planes = new()
     new Plane { Id = 2, Model = "Airbus A320", RegNumber = "CD-456", Capacity = 200, AirlineId = 2 },
     new Plane { Id = 3, Model = "Boeing 777", RegNumber = "EF-789", Capacity = 350, AirlineId = 3 },
     new Plane { Id = 4, Model = "Airbus A350", RegNumber = "GH-012", Capacity = 300, AirlineId = 4 },
-    new Plane { Id = 5, Model = "Embraer E190", RegNumber = "IJ-345", Capacity = 120, AirlineId = 5 }
+    new Plane { Id = 5, Model = "Embraer E190", RegNumber = "IJ-345", Capacity = 120, AirlineId = 5 },
+    new Plane { Id = 6, Model = "Boeing 777", RegNumber = "EF-790", Capacity = 300, AirlineId = 3 },
+    new Plane { Id = 7, Model = "Boeing 777", RegNumber = "EF-700", Capacity = 250, AirlineId = 3 },
+    new Plane { Id = 8, Model = "Airbus A320", RegNumber = "CD-123", Capacity = 150, AirlineId = 2 },
 };
 
 List<Route> routes = new()
@@ -47,12 +50,36 @@ List<RoutePlane> routePlanes = new()
 
 List<Flight> flights = new()
 {
-    new Flight { Id = 1, departureDateTime = DateTime.UtcNow.AddMonths(-5), realDepartureDateTime = DateTime.UtcNow.AddMonths(-5).AddMinutes(45), routeId = 1 },
-    new Flight { Id = 2, departureDateTime = DateTime.UtcNow.AddMonths(-4), realDepartureDateTime = DateTime.UtcNow.AddMonths(-4).AddMinutes(20), routeId = 1 },
-    new Flight { Id = 3, departureDateTime = DateTime.UtcNow.AddMonths(-3), realDepartureDateTime = DateTime.UtcNow.AddMonths(-3).AddMinutes(50), routeId = 2 },
-    new Flight { Id = 4, departureDateTime = DateTime.UtcNow.AddMonths(-2), realDepartureDateTime = DateTime.UtcNow.AddMonths(-2).AddMinutes(10), routeId = 2 },
-    new Flight { Id = 5, departureDateTime = DateTime.UtcNow.AddMonths(-1), realDepartureDateTime = DateTime.UtcNow.AddMonths(-1).AddMinutes(60), routeId = 3 },
-    new Flight { Id = 6, departureDateTime = DateTime.UtcNow.AddMonths(-7), realDepartureDateTime = DateTime.UtcNow.AddMonths(-7).AddMinutes(15), routeId = 3 }
+    new Flight
+    {
+        Id = 1, departureDateTime = DateTime.UtcNow.AddMonths(-5),
+        realDepartureDateTime = DateTime.UtcNow.AddMonths(-5).AddMinutes(45), routeId = 1
+    },
+    new Flight
+    {
+        Id = 2, departureDateTime = DateTime.UtcNow.AddMonths(-4),
+        realDepartureDateTime = DateTime.UtcNow.AddMonths(-4).AddMinutes(20), routeId = 1
+    },
+    new Flight
+    {
+        Id = 3, departureDateTime = DateTime.UtcNow.AddMonths(-3),
+        realDepartureDateTime = DateTime.UtcNow.AddMonths(-3).AddMinutes(50), routeId = 2
+    },
+    new Flight
+    {
+        Id = 4, departureDateTime = DateTime.UtcNow.AddMonths(-2),
+        realDepartureDateTime = DateTime.UtcNow.AddMonths(-2).AddMinutes(10), routeId = 2
+    },
+    new Flight
+    {
+        Id = 5, departureDateTime = DateTime.UtcNow.AddMonths(-1),
+        realDepartureDateTime = DateTime.UtcNow.AddMonths(-1).AddMinutes(60), routeId = 3
+    },
+    new Flight
+    {
+        Id = 6, departureDateTime = DateTime.UtcNow.AddMonths(-7),
+        realDepartureDateTime = DateTime.UtcNow.AddMonths(-7).AddMinutes(15), routeId = 3
+    }
 };
 
 List<Ticket> tickets = new()
@@ -67,8 +94,10 @@ List<Ticket> tickets = new()
     new Ticket { Id = 8, SeatClass = SeatClass.Business, PassengerId = 1, FlightId = 5 },
     new Ticket { Id = 9, SeatClass = SeatClass.Business, PassengerId = 2, FlightId = 5 },
     new Ticket { Id = 10, SeatClass = SeatClass.Business, PassengerId = 3, FlightId = 5 },
-    new Ticket { Id = 11, SeatClass = SeatClass.First, PassengerId = 4, FlightId = 5 }
+    new Ticket { Id = 11, SeatClass = SeatClass.First, PassengerId = 4, FlightId = 5 },
+    new Ticket { Id = 12, SeatClass = SeatClass.Economy, PassengerId = 2, FlightId = 4 }
 };
+
 
 // DateTime sixMonthsAgo = DateTime.Now.AddMonths(-6);
 // var delayedRoutes = from f in flights
@@ -84,6 +113,111 @@ List<Ticket> tickets = new()
 //     Console.WriteLine($"Маршрут: {route.origin} -> {route.destination}, Середня затримка: {route.AverageDelay} хв");
 // }
 
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+
+// фільтрація 
+var collection1 = from plane in planes
+    where plane.Model == "Boeing 777" && plane.Capacity >= 300
+    select plane;
+foreach (var plane in collection1)
+{
+    Console.WriteLine($"{plane.RegNumber} - {plane.Capacity}");
+}
+
+Console.WriteLine();
+
+// з'єднання
+var collection2 = from plane in planes
+    from airline in airlines
+    select new { regNumber = plane.RegNumber, airline = airline.Name };
+foreach (var item in collection2)
+{
+    Console.WriteLine($"{item.regNumber} - {item.airline}");
+}
+
+Console.WriteLine();
+
+// групування
+var collection3 = from plane in planes
+    join airline in airlines
+        on plane.AirlineId equals airline.Id
+    group plane by new { Id = plane.AirlineId, country = airline.Country };
+
+foreach (var plane in collection3)
+{
+    Console.WriteLine($"{plane.Key.country} - {plane.Count()}");
+}
+
+Console.WriteLine();
+
+// групування з фільтрацією
+
+// var collection4 = passengers.Join(tickets, passenger => passenger.Id, ticket => ticket.PassengerId,
+//         (passenger, ticket) => (passenger.Age, ticket.SeatClass))
+//     .Where(ticket => ticket.SeatClass == SeatClass.Business)
+//     .GroupBy(passenger =>
+//     {
+//         if (passenger.Age >= 25 && passenger.Age <= 27)
+//         {
+//             return "Молодші";
+//         }
+//
+//         if (passenger.Age >= 28 && passenger.Age <= 32)
+//         {
+//             return "Середні";
+//         }
+//
+//         if (passenger.Age >= 33 && passenger.Age <= 36)
+//         {
+//             return "Старші";
+//         }
+//
+//         return "Інші";
+//     });
+
+var collection4 = passengers
+    .Where(p => tickets.Any(t => t.PassengerId == p.Id && t.SeatClass == SeatClass.Business)) // Фільтр пасажирів, які мають квитки бізнес-класу
+    .GroupBy(p =>
+    {
+        return p.Age switch
+        {
+            >= 25 and <= 27 => "Молодші",
+            >= 28 and <= 32 => "Середні",
+            >= 33 => "Старші",
+            _ => "Інші"
+        };
+    })
+    .Select(group => new 
+    {
+        AgeGroup = group.Key,
+        Passengers = group.ToList()
+    })
+    .ToList();
+
+foreach (var group in collection4)
+{
+    foreach (var passenger in group.Passengers)
+    {
+        Console.WriteLine($"{passenger.Surname} - {group.AgeGroup}");
+    }
+}
+Console.WriteLine();
+
+
+
+// сортування з фільтрацією
+var collection6 = from passenger in passengers
+    where passenger.Age is >= 20 and <= 30
+    orderby passenger.Age descending
+    select passenger;
+foreach (var passenger in collection6)
+{
+    Console.WriteLine($"{passenger.Surname} - {passenger.Age}");
+}
+
+Console.WriteLine();
+
 
 public enum SeatClass
 {
@@ -91,12 +225,14 @@ public enum SeatClass
     Business,
     First
 }
+
 public class Airline
 {
     public int Id { get; set; }
     public string Name { get; set; }
     public string Country { get; set; }
 }
+
 public class Flight
 {
     public int Id { get; set; }
@@ -104,6 +240,7 @@ public class Flight
     public DateTime realDepartureDateTime { get; set; }
     public int routeId { get; set; }
 }
+
 public class Passenger
 {
     public int Id { get; set; }
@@ -111,6 +248,7 @@ public class Passenger
     public string Surname { get; set; }
     public int Age { get; set; }
 }
+
 public class Plane
 {
     public int Id { get; set; }
@@ -119,17 +257,20 @@ public class Plane
     public int Capacity { get; set; }
     public int AirlineId { get; set; }
 }
+
 public class Route
 {
     public int Id { get; set; }
     public string Origin { get; set; }
     public string Destination { get; set; }
 }
+
 public class RoutePlane
 {
     public int PlaneId { get; set; }
     public int RouteId { get; set; }
 }
+
 public class Ticket
 {
     public int Id { get; set; }
